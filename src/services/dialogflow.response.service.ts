c076fb14325cca49bd1aecd16f12f2e81bb26f40
Rohitkemprai93 <rohit.kemprai@kansocloud.com>
2021-10-29 14:31:59 +0530

    Update dialogflow.response.service.ts

--- src/services/dialogflow.response.service.ts
+++ src/services/dialogflow.response.service.ts
@@ -1,76 +1,41 @@
 import dialogflow from '@google-cloud/dialogflow';
 import { v4 } from 'uuid';
 import { injectable } from 'tsyringe';
-import { v2 } from '@google-cloud/translate';
-import { text } from 'body-parser';
-import { response } from 'express';
-import path from 'path';
-import { combineTableNames } from 'sequelize/types/lib/utils';
-const projectId = process.env.DIALOGFLOW_PROJECT_ID;
-const appSupportProjectId = process.env.DIALOGFLOW_PROJECT_ID_REAN_APP;
 const translateProjectId = process.env.TRANSLATE_PROJECT_ID;
-// A unique identifier for the given session
-
 
 @injectable()
 export class DialogflowResponseService {
     getDialogflowMessage = async (message, userSessionId = null, platform = null) => {
-        console.log("this is message from getdialogflow................", message)
         try {
             // set default values
-            let detected_language = 'en';
-            let responseMessage = { text: [], parse_mode: false, image: { url: '', caption: '' } };
+            let detected_language = 'en';        
+            let responseMessage = { text: [], parse_mode: false, image: { url: '', caption:''} };
             let dialogflow_language = "en-US";
             const sessionId = userSessionId == null ? v4() : userSessionId;
-
+            
             let sessionClient = null;
             let sessionPath = null;
             let translate = null
+            let options = {};
+            let projectIdFinal =null;
+
             if (platform == "REAN_SUPPORT") {
                 translate = false
-
-                // init session client for DF using manual explicit file for REAN Support App Bot
-                // check docs here: node_modules/dialogflow/src/v2/sessions_client.js
-                let options = {
+                options = {
                     keyFilename: process.env.REAN_APP_SUPPORT_GCP_PROJ_CREDENTIALS
                 }
-                sessionClient = new dialogflow.SessionsClient(options);
-
-                sessionPath = sessionClient.projectAgentSessionPath(appSupportProjectId, sessionId);
+                projectIdFinal = process.env.DIALOGFLOW_PROJECT_ID_REAN_APP;
+                
             } else {
                 console.log("Entered the else of Dialogflow..............")
-                // condition to use same / different GCP for dialogflow and transation api
-                const dialogflowApplicationCredentialsFile = process.env.DIALOGFLOW_BOT_GCP_PROJECT_CREDENTIALS ? process.env.DIALOGFLOW_BOT_GCP_PROJECT_CREDENTIALS : process.env.GOOGLE_APPLICATION_CREDENTIALS;
-                // init session client for DF using manual explicit file
-                let options = {
-                    keyFilename: dialogflowApplicationCredentialsFile
+                options = {
+                    keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS
                 }
-                sessionClient = new dialogflow.SessionsClient(options);
-
-                // translate = await initializeTranslate(translateProjectId); ////////////////
-                sessionPath = sessionClient.projectAgentSessionPath(projectId, sessionId);
+                projectIdFinal = process.env.DIALOGFLOW_PROJECT_ID;;
+                
             }
-            ///////////////////////
-            // if (translate) {
-            //     let [detections] = await translate.detect(message);
-            //     console.log("this is detections", translate.detect(message))
-            //     detections = Array.isArray(detections) ? detections : [detections];
-            //     console.log("this is 2nd line detections", detections)
-
-            //     detected_language = detections[0].language;
-            //     console.log("the language is", detected_language)
-            //     if (detected_language != 'en') {
-            //         const target = 'en';
-            //         let [translation] = await translate.translate(message, target);
-            //         message = translation;
-            //         dialogflow_language = "en-US";
-            //     }
-            // }
-            //////////////////////////
-
-            // let messagefordialogflow;
-            // messagefordialogflow = await translateMessage(message)
-
+            sessionClient = new dialogflow.SessionsClient(options);
+            sessionPath = sessionClient.projectAgentSessionPath(projectIdFinal, sessionId);
             console.log("Message to be sent to DF: ", message)
             const request = {
                 session: sessionPath,
@@ -81,7 +46,7 @@ export class DialogflowResponseService {
                     },
                 },
             };
-
+            console.log("B$session CLient detects intent")
             const responses = await sessionClient.detectIntent(request);
             const result = responses[0].queryResult;
             if (result.intent) {
@@ -89,20 +54,15 @@ export class DialogflowResponseService {
             } else {
                 // console.log(`  No intent matched.`);
             }
+
+            console.log("B$getFulfillmentResponse gets response")
             responseMessage = getFulfillmentResponse(result);
             if (responseMessage.text.length == 0) {
                 responseMessage.text[0] = result.fulfillmentText;
             }
-            // let translatedResponse; /////////////////
-            // if (responseMessage.parse_mode) {
-            //     translatedResponse = responseMessage.text;
-            // }
-            // else {
-            //     translatedResponse = await translateResponse(translate, responseMessage.text, detected_language)
-            // }//////////////////
-            console.log("returned dialogflow,", responseMessage)
+            console.log("returned dialogflow,",responseMessage )
             return {
-                text: responseMessage, //responseMessage
+                text: responseMessage,
                 image: responseMessage.image ? responseMessage.image : false,
                 parse_mode: responseMessage.parse_mode ? responseMessage.parse_mode : false,
                 result: result,
@@ -112,46 +72,19 @@ export class DialogflowResponseService {
             console.log(e)
             return {
                 text: ["Sorry, something went wrong. Let me consult an expert and get back to you!"],
-                image: { url: '', caption: '' },
+                image:  {url: '', caption:''},
                 parse_mode: false,
                 result: false
             };
         }
-
-
+    
+    
     }
 
 }
-//
-// async function initializeTranslate(translateProjectId) {
-//     // return false;
-//     try {
-//         const translateObj = new v2.Translate({projectId: translateProjectId });
-//         return translateObj
-//     }
-//     catch (e) {
-//         return false;
-//     }
-// }
-// //
-// async function translateResponse(translate, responseMessage, detected_language) {
-//     try {
-//         if (detected_language != 'en') {
-//             console.log("It is not english loop")
-//             let [translation] = await translate.translate(responseMessage[0], { to: detected_language, format: "text" });
-//             responseMessage = [translation];
-//         }
-//         return responseMessage;
-//     } catch (e) {
-//         console.log("catch translate", e);
-//         return responseMessage;
-//     }
-// }
-//
-
 
 function getFulfillmentResponse(result) {
-    let responseMessage = { text: [], parse_mode: false, image: { url: '', caption: '' } };
+    let responseMessage = { text: [], parse_mode: false, image: { url: '', caption:''}};
 
     if (result.fulfillmentMessages) {
         if (result.fulfillmentMessages[0].platform && result.fulfillmentMessages[0].platform == "TELEGRAM" && result.fulfillmentMessages[0].payload) {
