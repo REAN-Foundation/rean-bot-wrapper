437982e3b3da058d4e50c3d9d76403976570c25b
Rohitkemprai93 <rohit.kemprai@kansocloud.com>
2021-11-03 15:04:09 +0530

    removed dublication from /common/response.handler

--- src/common/response.handler.ts
+++ src/common/response.handler.ts
@@ -6,27 +6,12 @@ import { ResponseDto } from '../domain.types/miscellaneous/response.dto';
 
 export class ResponseHandler {
 
-    public static failure(
-        request: any,
-        response: express.Response,
-        message?: string,
-        httpErrorCode?: number,
-        error?: Error
-    ) {
-        const ips = [
-            request.header('x-forwarded-for') || request.socket.remoteAddress
-        ];
-
-        const msg = error ? error.message : (message ? message : 'An error has occurred.');
-
-        const errorStack = error ? error.stack : '';
-        const tmp = errorStack.split('\n');
-        const trace_path = tmp.map(x => x.trim());
-
+    public static createResponseObject (status,message,httpCode,data,trace_path,request,ips) {
         const responseObject: ResponseDto = {
-            Status   : 'failure',
-            Message  : msg,
-            HttpCode : httpErrorCode ? httpErrorCode : 500,
+            Status   : status,
+            Message  : message,
+            HttpCode : httpCode ?? 200,
+            Data     : data,
             Trace    : trace_path,
             Client   : request ? request.currentClient : null,
             User     : request ? request.currentUser : null,
@@ -43,6 +28,28 @@ export class ResponseHandler {
             APIVersion     : process.env.API_VERSION,
             ServiceVersion : process.env.SERVICE_VERSION,
         };
+        return responseObject;
+    }
+
+    public static failure(
+        request: any,
+        response: express.Response,
+        message?: string,
+        httpErrorCode?: number,
+        error?: Error
+    ) {
+        const ips = [
+            request.header('x-forwarded-for') || request.socket.remoteAddress
+        ];
+
+        const msg = error ? error.message : (message ? message : 'An error has occurred.');
+
+        const errorStack = error ? error.stack : '';
+        const tmp = errorStack.split('\n');
+        const trace_path = tmp.map(x => x.trim());
+        const data = null;
+
+        const responseObject = this.createResponseObject("failure",msg,httpErrorCode,data,trace_path,request,ips);
 
         if (process.env.NODE_ENV !== 'test') {
             Logger.instance().log(JSON.stringify(responseObject, null, 2));
@@ -67,27 +74,9 @@ export class ResponseHandler {
             request.header('x-forwarded-for') || request.socket.remoteAddress
         ];
 
-        const responseObject: ResponseDto = {
-            Status   : 'success',
-            Message  : message,
-            HttpCode : httpCode ?? 200,
-            Data     : data ?? null,
-            Trace    : null,
-            Client   : request ? request.currentClient : null,
-            User     : request ? request.currentUser : null,
-            Context  : request ? request.context : null,
-            Request  : {
-                Method  : request ? request.method : null,
-                Host    : request ? request.hostname : null,
-                Body    : request ? request.body : null,
-                Headers : request ? request.headers : null,
-                Url     : request ? request.originalUrl : null,
-                Params  : request ? request.params : null,
-            },
-            ClientIps      : request && request.ips.length > 0 ? request.ips : ips,
-            APIVersion     : process.env.API_VERSION,
-            ServiceVersion : process.env.SERVICE_VERSION,
-        };
+        const Trace = null;
+
+        const responseObject = this.createResponseObject("success",message,httpCode,data,Trace,request,ips);
 
         if (process.env.NODE_ENV !== 'test') {
             if (!logDataObject) {
