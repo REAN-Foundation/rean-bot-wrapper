4f1000dc45c366127aaab99285d86bf951d420d4
rahulmaske77 <86289009+rahulmaske77@users.noreply.github.com>
2021-11-12 14:55:35 +0530

    entrypoint updated (#14)
    Co-authored-by: Rahul Maske <rahulmaske77@gmail>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@
 aws s3 cp s3://$S3_CONFIG_BUCKET/rean_bot/env.config /app/.env
 aws s3 cp s3://$S3_CONFIG_BUCKET/rean_bot/service_account_key_dialogflow_translation.json /app/service_account_key_dialogflow_translation.json
 aws s3 cp s3://$S3_CONFIG_BUCKET/rean_bot/rean-healthguru-development-reanhealthguru.json /app/rean-healthguru-development-reanhealthguru.json
+aws s3 cp s3://$S3_CONFIG_BUCKET/rean_bot/service_account_key_rean_app_support_bot.json /app/service_account_key_rean_app_support_bot.json
 
 
 cd /app
