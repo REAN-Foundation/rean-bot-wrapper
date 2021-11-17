dc05beb5e241714349189261c478591ae21ec30d
Rohitkemprai93 <rohit.kemprai@kansocloud.com>
2021-11-17 15:14:09 +0530

    Update elasticsearchmodel.ts

--- src/models/elasticsearchmodel.ts
+++ src/models/elasticsearchmodel.ts
@@ -2,12 +2,13 @@ import { elasticsearchUtilities } from '../utils/elasticsearch.utility';
 
 const esinstance = new elasticsearchUtilities();
 export class IndexCreation {
+
     createIndexes() {
         if (esinstance.client) {
             const index_prefix = process.env.ELASTICSEARCH_INDEX_PREFIX ? process.env.ELASTICSEARCH_INDEX_PREFIX : 'test_';
             console.log("creating index in ES");
             esinstance.client.indices.create({
-                index: index_prefix + 'chat_message'
+                index : index_prefix + 'chat_message'
             }, function (err, resp) {
                 if (err) {
                     console.log(err);
@@ -17,5 +18,6 @@ export class IndexCreation {
                 }
             });
         }
-    };
+    }
+
 }
