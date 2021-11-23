bb3c1ed9ea260053f5019f4ba037a30dab56b74f
Rohitkemprai93 <rohit.kemprai@kansocloud.com>
2021-11-23 18:29:07 +0530

    Update telegram.message.service.ts

--- src/services/telegram.message.service.ts
+++ src/services/telegram.message.service.ts
@@ -26,22 +26,6 @@ export class platformMessageService implements platformServiceInterface{
         const client = null;
         this.init(client);
     }
-    
-    // emojiUnicode = async (emoji) => {
-    //     // eslint-disable-next-line init-declarations
-    //     let comp;
-    //     if (emoji.length === 1) {
-    //         comp = emoji.charCodeAt(0);
-    //     }
-    //     comp = (
-    //         (emoji.charCodeAt(0) - 0xD800) * 0x400
-    //         + (emoji.charCodeAt(1) - 0xDC00) + 0x10000
-    //     );
-    //     if (comp < 0) {
-    //         comp = emoji.charCodeAt(0);
-    //     }
-    //     return comp.toString("16");
-    // };
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     handleMessage(msg, client: string){
@@ -72,18 +56,6 @@ export class platformMessageService implements platformServiceInterface{
         const chat_message_id = message.message_id;
         if (message.text) {
             message = await this.emojiFilter.checkForEmoji(message.text);
-
-            // let msgToUnicode = await this.emojiUnicode(message.text);
-            // if (msgToUnicode === '1f44d'){
-            //     message = "PositiveFeedback";
-            // }
-            // else if (msgToUnicode === "1f44e"){
-            //     message = "NegativeFeedback";
-            // }
-            // else{
-            //     message = message.text;
-            // }
-
             returnMessage = { name: name, platform: "Telegram",chat_message_id: chat_message_id,direction: "In",messageBody: message,sessionId: telegram_id,replayPath: telegram_id,latlong: null,type: 'text' };
         }
         else if (message.voice) {
@@ -215,42 +187,5 @@ export class platformMessageService implements platformServiceInterface{
         }
         return message;
     }
-
-    // checkForEmoji = async (message) => {
-    //     const regex = emojiRegex();
-    //     let positiveEmoji: String[] = ['1f44d', '1f604', '1f601'];
-    //     let negativeEmoji: String[] = ['1f44e', '1f621', '1f92c'];
-    //     // let emoji: String[] = [];
-    //     let UnicodeEmoji: String[] = [];
-    //     for (const match of message.matchAll(regex)) {
-    //         // emoji.push(match)
-    //         let convertToUnicodeEmoji = await this.emojiUnicode(match[0]);
-    //         UnicodeEmoji.push(convertToUnicodeEmoji);
-    //         console.log("the emoji is ", UnicodeEmoji);
-    //     }
-
-    //     if (UnicodeEmoji !== null) {
-    //         let missingPositive = positiveEmoji.filter(item => UnicodeEmoji.indexOf(item) < 0);
-    //         console.log("missing positive", missingPositive);
-    //         let missingnegative = negativeEmoji.filter(item => UnicodeEmoji.indexOf(item) < 0);
-    //         console.log("missing negative", missingnegative);
-    //         if (missingPositive.length <= (positiveEmoji.length - 1)) {
-    //             message = "PositiveFeedback";
-    //         }
-    //         else {
-    //             console.log("No positive emoji");
-    //         }
-    //         if (missingnegative.length <= (negativeEmoji.length - 1)) {
-    //             message = "NegativeFeedback";
-    //         }
-    //         else {
-    //             console.log("No negative emoji");
-    //         }
-    //     }
-    //     else {
-    //         message = message;
-    //     }
-    //     return message
-    // }
 
 }
