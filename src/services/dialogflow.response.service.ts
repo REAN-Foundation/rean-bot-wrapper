db16f4d6c7e725e31c9e777c749cadaa3a3f0979
Rohitkemprai93 <rohit.kemprai@kansocloud.com>
2021-11-29 17:35:42 +0530

    removed console logs

--- src/services/dialogflow.response.service.ts
+++ src/services/dialogflow.response.service.ts
@@ -34,9 +34,7 @@ export class DialogflowResponseService {
 
             }
             sessionClient = new dialogflow.SessionsClient(options);
-            console.log(`the dialogflow.sessionClient is  ${sessionClient}`);
             sessionPath = sessionClient.projectAgentSessionPath(projectIdFinal, sessionId);
-            console.log(`the dialogflow.sessionClient.projectAgentSessionPath is  ${sessionPath}`);
             console.log("Message to be sent to DF: ", message);
             const request = {
                 session    : sessionPath,
@@ -49,7 +47,6 @@ export class DialogflowResponseService {
             };
             console.log("B$session CLient detects intent");
             const responses = await sessionClient.detectIntent(request);
-            console.log(`the dialogflow.sessionClient.detectIntent is  ${responses}`);
             const result = responses[0].queryResult;
             if (result.intent) {
                 console.log(`  Intent: ${result.intent.displayName}`);
