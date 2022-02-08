f9e1da1f33599e0ec4d4fe2f68fe6ee8ca62b0ca
vinay-debug <2020bmm1015@iitrpr.ac.in>
2022-02-08 18:12:42 +0530

    change in biometrics header file

--- src/services/biometrics/get.headers.ts
+++ src/services/biometrics/get.headers.ts
@@ -9,7 +9,8 @@ export const getHeaders = (accessToken: any) => {
     const reancare_api_key = clientEnvironmentProviderService.getClientEnvironmentVariable("ReancareApiKey");
     const options = getRequestOptions("rean_app");
     options.headers["authorization"] = `Bearer ${accessToken}`;
-    options.headers["x-api-key"] = `${reancare_api_key}`;
+    options.headers["x-api-key"] = reancare_api_key;
+    console.log('reancareAPIkEY for dev services', reancare_api_key);
     console.log('headers in the options', options);
     return options;
 };
