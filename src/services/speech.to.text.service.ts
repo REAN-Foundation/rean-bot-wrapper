8535399915344b42233874d0a5c2a46806c4eea2
Rohitkemprai93 <rohit.kemprai@kansocloud.com>
2022-08-01 18:13:26 +0530

    audio issue due to signed url

--- src/services/speech.to.text.service.ts
+++ src/services/speech.to.text.service.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable max-len */
 import http from 'https';
 import path from 'path';
 import speech from '@google-cloud/speech';
@@ -5,28 +7,28 @@ import fs from 'fs';
 import { AwsS3manager } from './aws.file.upload.service';
 import { autoInjectable } from 'tsyringe';
 import { ClientEnvironmentProviderService } from './set.client/client.environment.provider.service';
-
 @autoInjectable()
 export class Speechtotext {
 
     constructor(
         private awss3manager?: AwsS3manager,
-        private clientEnvironmentProviderService?: ClientEnvironmentProviderService) { }
+        private clientEnvironmentProviderService?: ClientEnvironmentProviderService) {
+        this.GCPCredentials = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+        this.obj_gcp = {
+            credentials : this.GCPCredentials,
+            projectId   : this.GCPCredentials.project_id
+        };
+        this.env = this.clientEnvironmentProviderService.getClientEnvironmentVariable("ENVIRONMENT");
+    }
 
-    private GCPCredentials = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+    private GCPCredentials;
 
-    private awsCred = this.awss3manager;
+    private env;
 
-    private env = this.clientEnvironmentProviderService.getClientEnvironmentVariable("ENVIRONMENT");
-
-    private obj_gcp = {
-        credentials : this.GCPCredentials,
-        projectId   : this.GCPCredentials.project_id
-    };
+    private obj_gcp;
 
     async SendSpeechRequest(fileUrl, chatServiceName, preferredLanguage) {
         return new Promise(async (resolve, reject) => {
-            const obj = this.obj_gcp;
             if (chatServiceName === 'telegram') {
                 http.get(fileUrl, async (res) => {
                     
@@ -40,56 +42,53 @@ export class Speechtotext {
                     res.pipe(filePath);
 
                     const awsFile = await this.awss3manager.uploadFile(uploadpath);
-                    main(awsFile, obj,this.awsCred,this.env, preferredLanguage).catch(console.error);
+                    resolve(await this.callGCPSpeech(awsFile,this.env, preferredLanguage));
                 });
             } else if (chatServiceName === 'whatsapp') {
                 const awsFile = await this.awss3manager.uploadFile(fileUrl);
-                main(awsFile, obj,this.awsCred,this.env, preferredLanguage).catch(console.error);
+                resolve(await this.callGCPSpeech(awsFile,this.env, preferredLanguage).catch(console.error));
             }
+        });
+    }
 
-            async function main(uploadpath, obj, awscred,env, preferredLanguage) {
-                try {
-                    const environment = {
-                        'DEVELOPMENT' : 'dev',
-                        'UAT'         : 'uat',
-                        'PROD'        : 'prod'
-                    };
+    async callGCPSpeech(uploadpath, env, preferredLanguage) {
+        return new Promise(async(resolve,reject) =>{
+            try {
+                const gcpCred = this.obj_gcp;
+                const client = new speech.SpeechClient(gcpCred);
+                const url = require('url');
+                const key = ((url.parse(uploadpath)).pathname).substring(1);
 
-                    const client = new speech.SpeechClient(obj);
+                const awsGetFile = await this.awss3manager.getFile(key);
 
-                    const key = environment[env] + '/' + uploadpath.split('/')[4];
+                const audioBytes = awsGetFile.Body.toString('base64');
 
-                    const awsGetFile = await awscred.getFile(key);
-                    const audioBytes = awsGetFile.Body.toString('base64');
-                    
-                    console.log("upload Path,", uploadpath);
+                const audio = {
+                    content : audioBytes,
+                };
 
-                    const audio = {
-                        content : audioBytes,
-                    };
+                const config = {
+                    encoding                            : "OGG_OPUS",
+                    sampleRateHertz                     : 48000,
+                    languageCode                        : preferredLanguage,
+                    enableSeparateRecognitionPerChannel : true,
+                };
+                let request = {};
+                request = {
+                    audio  : audio,
+                    config : config,
+                };
 
-                    const config = {
-                        encoding                            : "OGG_OPUS",
-                        sampleRateHertz                     : 16000,
-                        languageCode                        : preferredLanguage,
-                        enableSeparateRecognitionPerChannel : true,
-                    };
-                    let request = {};
-                    request = {
-                        audio  : audio,
-                        config : config,
-                    };
+                const [response] = await client.recognize(request);
 
-                    const [response] = await client.recognize(request);
-
-                    // console.log("response %O", response);
-                    const transcription = response.results
-                        .map(result => result.alternatives[0].transcript)
-                        .join('\n');
-                    resolve(transcription);
-                } catch (error) {
-                    reject(error);
-                }
+                // console.log("response %O", response);
+                const transcription = response.results
+                    .map(result => result.alternatives[0].transcript)
+                    .join('\n');
+                console.log("transcription", transcription);
+                resolve(transcription);
+            } catch (error) {
+                reject(error);
             }
         });
     }
