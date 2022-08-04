3c1881bac9f8e90ed42d7f894fff355a63afdaf0
Rohitkemprai93 <rohit.kemprai@kansocloud.com>
2022-08-04 13:14:23 +0530

    cronjob for feedbach script added but not implemented

--- src/services/get.put.message.flow.service.ts
+++ src/services/get.put.message.flow.service.ts
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 /* eslint-disable linebreak-style */
 import { Imessage, Iresponse, IchatMessage } from '../refactor/interface/message.interface';
+
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { handleRequestservice } from './handle.request.service';
 import { autoInjectable } from 'tsyringe';
@@ -11,6 +12,7 @@ import { CallAnemiaModel } from './call.anemia.model';
 import { GoogleTextToSpeech } from './text.to.speech';
 import { UserFeedback } from '../models/user.feedback.model';
 import { SlackMessageService } from "./slack.message.service";
+import { ChatSession } from '../models/chat.session';
 
 @autoInjectable()
 export class MessageFlow{
@@ -25,22 +27,11 @@ export class MessageFlow{
     async checkTheFlow(msg: any, channel: string, platformMessageService: platformServiceInterface){
         const messagetoDialogflow: Imessage = await platformMessageService.getMessage(msg);
         console.log("message to DF", messagetoDialogflow);
-        const chatMessageObj: IchatMessage = {
-            name           : messagetoDialogflow.name,
-            platform       : messagetoDialogflow.platform,
-            direction      : messagetoDialogflow.direction,
-            messageType    : messagetoDialogflow.type,
-            messageContent : messagetoDialogflow.messageBody,
-            imageContent   : null,
-            imageUrl       : messagetoDialogflow.imageUrl,
-            userPlatformID : messagetoDialogflow.sessionId,
-            intent         : null
-        };
-        await this.sequelizeClient.connect();
-        const personrequest = new ChatMessage(chatMessageObj);
-        await personrequest.save();
-        const userId = chatMessageObj.userPlatformID;
-        const resp = await UserFeedback.findAll({ where: { userId: userId } });
+
+        //initialising MySQL DB tables
+        const chatMessageObj = await this.engageMySQL(messagetoDialogflow);
+        
+        const resp = await UserFeedback.findAll({ where: { userId: chatMessageObj.userPlatformID } });
         if (resp.length === 0) {
             this.get_put_msg_Dialogflow(messagetoDialogflow, channel, platformMessageService);
         }
@@ -139,6 +130,50 @@ export class MessageFlow{
         // eslint-disable-next-line max-len
         message_to_platform = await platformMessageService.SendMediaMessage(response_format.sessionId, response_format.messageBody,response_format.messageText, response_format.message_type);
         return message_to_platform;
+    }
+
+    async engageMySQL(messagetoDialogflow) {
+        return new Promise<IchatMessage>(async(resolve) =>{
+            const chatMessageObj: IchatMessage = {
+                name           : messagetoDialogflow.name,
+                platform       : messagetoDialogflow.platform,
+                direction      : messagetoDialogflow.direction,
+                messageType    : messagetoDialogflow.type,
+                messageContent : messagetoDialogflow.messageBody,
+                imageContent   : null,
+                imageUrl       : messagetoDialogflow.imageUrl,
+                userPlatformID : messagetoDialogflow.sessionId,
+                intent         : null
+            };
+            await this.sequelizeClient.connect();
+            const personrequest = new ChatMessage(chatMessageObj);
+            await personrequest.save();
+            const userId = chatMessageObj.userPlatformID;
+            const respChatSession = await ChatSession.findAll({ where: { userPlatformID: userId } });
+            const respChatMessage = await ChatMessage.findAll({ where: { userPlatformID: userId } });
+            const lastMessageDate = respChatMessage[respChatMessage.length - 1].createdAt;
+
+            // console.log("lastMessageDate",lastMessageDate);
+            // console.log("respChatSession!!!", respChatSession);
+            if (respChatSession.length === 0 || respChatSession[respChatSession.length - 1].sessionOpen === "false") {
+
+                // console.log("starting a new session");
+                const newChatsession = new ChatSession({ userPlatformID  : messagetoDialogflow.sessionId,
+                    platform        : messagetoDialogflow.platform, sessionOpen     : "true",
+                    lastMessageDate : lastMessageDate, askForFeedback  : "flase" });
+
+                // console.log("newChatsession", newChatsession);
+                await newChatsession.save();
+            }
+            else {
+                const autoIncrementalID = respChatSession[respChatSession.length - 1].autoIncrementalID;
+                await ChatSession.update({ lastMessageDate: lastMessageDate }, { where: { autoIncrementalID: autoIncrementalID } } )
+                    .then(() => { console.log("updated lastMessageDate"); })
+                    .catch(error => console.log("error on update", error));
+            }
+            resolve(chatMessageObj);
+        });
+        
     }
 
 }
