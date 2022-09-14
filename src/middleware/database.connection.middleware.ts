9bf670542eca3e42081c3cb21b24c6376a917d10
sushant-thotakura <95743770+sushant-thotakura@users.noreply.github.com>
2022-09-14 18:00:40 +0530

    Bugs/whatsapp image urls (#146)
    * Bug Fixes
    
    Fix:
     - Regex to detect url in whatsapp messages
    
    * Fix to regex
    
    * Whatsapp meta fixed
    
    * Whatsapp Telegram Fix

--- src/middleware/database.connection.middleware.ts
+++ src/middleware/database.connection.middleware.ts
@@ -7,10 +7,19 @@ export class DatabaseConnection{
     constructor(private sequelizeClient?: SequelizeClient){}
 
     myLogger = async (req, res, next) => {
-        if (req.body.statuses || req.body.entry[0].changes[0].value.statuses){
-            console.log("DO NOTHING");
-        } else {
+        if (!req.body.statuses){
             await this.sequelizeClient.connect();
+        } else {
+            console.log("360Dialog & Telegram Connection Avoided");
+        }
+        next();
+    }
+
+    metaDBConnection = async (req, res, next) => {
+        if (!req.body.entry[0].changes[0].value.statuses){
+            await this.sequelizeClient.connect();
+        } else {
+            console.log("Meta DB Connection Skipped");
         }
         next();
     }
