369d3fac58de8e69a50488631b67456901aee0e3
tabbasum-rean <tabbasum.kolhapure@reanfoundation.org>
2022-10-31 15:54:16 +0530

    Fixed code duplication

--- src/services/sneha.support.service.ts
+++ src/services/sneha.support.service.ts
@@ -8,31 +8,22 @@ import { ResponseHandler } from '../utils/response.handler';
 @singleton()
 export class snehaMessagePlatformService implements platformServiceInterface{
 
-    public translate = false;
-
-    public res;
+    public response;
 
     constructor(private messageFlow?: MessageFlow,private responseHandler?: ResponseHandler,
     ) {
 
     }
-
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    setWebhook(client: any) {
-        throw new Error('Method not implemented.');
+    sendManualMesage(msg: any) {
+        console.log("Method not implemented");
     }
 
     init() {
-        throw new Error('Method not implemented.');
+        console.log("Method not implemented");   
     }
 
-    sendManualMesage() {
-        throw new Error('Method not implemented.');
-    }
-
-    createFinalMessageFromHumanhandOver() {
-        throw new Error('Method not implemented.');
-    }
+    setWebhook(client: any) {
+        console.log("Method not implemented");    }
 
     handleMessage(msg, client) {
         return this.messageFlow.checkTheFlow(msg, client, this);
@@ -40,30 +31,30 @@ export class snehaMessagePlatformService implements platformServiceInterface{
 
     getMessage(msg) {
         // eslint-disable-next-line init-declarations
-        let returnMessage: Imessage;
-        const phoneNumber = msg.phoneNumber.toString();
+        let returnMsg: Imessage;
+        const phone = msg.phoneNumber.toString();
 
         if (msg.type === "text") {
             const message = msg.message; //+ ` PhoneNumber is ${phoneNumber}`;
-            returnMessage = { name: null,platform: "Sneha_Support",chat_message_id: null, direction: "In",messageBody: message,imageUrl: null, sessionId: phoneNumber,replyPath: phoneNumber,latlong: null,type: 'text' , intent: null};
-            return returnMessage;
+            returnMsg = { name: null,platform: "Sneha_Support",chat_message_id: null, direction: "In",messageBody: message,imageUrl: null, sessionId: phone,replyPath: phone,latlong: null,type: 'text' , intent: null};
+            return returnMsg;
         }
     }
 
     postResponse (message, response: IprocessedDialogflowResponseFormat ){
-        const reansupport_Id = message.sessionId;
+        const snehaSupport_Id = message.sessionId;
         const image = response.message_from_dialoglow.getImageObject();
-        const message_type = image.url ? "image" : "text";
+        const messageType = image.url ? "image" : "text";
         // const raw_response_object = response.message_from_dialoglow.result && response.message_from_dialoglow.result.fulfillmentMessages ? JSON.stringify(response.message_from_dialoglow.result.fulfillmentMessages) : '';
         const intent = response.message_from_dialoglow.getIntent();
 
-        const reaponse_message = { name: null,platform: "Sneha_Support",chat_message_id: null,direction: "Out",message_type: message_type,intent: intent,messageBody: null, messageImageUrl: null , messageImageCaption: null, sessionId: reansupport_Id, messageText: response.processed_message[0] };
-        return reaponse_message;
+        const responseMessage = { name: null,platform: "Sneha_Support",chat_message_id: null,direction: "Out",message_type: messageType,intent: intent,messageBody: null, messageImageUrl: null , messageImageCaption: null, sessionId: snehaSupport_Id, messageText: response.processed_message[0] };
+        return responseMessage;
 
     }
 
     SendMediaMessage(contact,imageLink, message){
-        this.responseHandler.sendSuccessResponseForApp(this.res, 201, "Message processed successfully.", { response_message: message });
+        this.responseHandler.sendSuccessResponseForApp(this.response, 201, "Message processed successfully.", { response_message: message });
         return message;
     }
 
