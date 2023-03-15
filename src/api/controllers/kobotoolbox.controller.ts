ad7282834f344969659bbc4e6b514f00ce8b2900
Pallavi17101997 <112403629+Pallavi17101997@users.noreply.github.com>
2023-03-15 08:58:13 +0530

    Kobotoolbox response capture (#257)
    * data_saving_pipeline
    
    created a separate route to save data in s3 which is coming from kobotoolbox
    
    * Kobotoolbox_pipeline
    
    pipeline to store the data coming from kobotoolbox to S3
    
    * solved linting issues
    
    removed errors
    
    ---------
    
    Co-authored-by: Pallavi <pallavisachdeva1710@gmail.com>

---
+++ src/api/controllers/kobotoolbox.controller.ts
@@ -0,0 +1,65 @@
+import { ResponseHandler } from '../../utils/response.handler';
+import { autoInjectable } from 'tsyringe';
+import { ClientEnvironmentProviderService } from '../../services/set.client/client.environment.provider.service';
+import { AwsS3manager } from '../../services/aws.file.upload.service';
+@autoInjectable()
+export class kobotoolboxController{
+    constructor(
+        private responseHandler?: ResponseHandler,
+        private clientEnvironment?: ClientEnvironmentProviderService,
+        private awss3manager?: AwsS3manager) {
+
+    }
+
+    private reformedData = {};
+
+    async getKeys(req,fileName){
+        const clientName = req.params.client;
+        const folderName = req.params.form_name;
+        const getFileKey = `${clientName}/${folderName}/datastructure/datastructure.json`;
+        const uploadFileKey = `${clientName}/${folderName}/data/${fileName}.json`;
+        return [getFileKey, uploadFileKey];
+    }
+
+    async getdatastructure(getFileKey){
+        const awsFile = await this.awss3manager.getFile(getFileKey);
+        const datastructure = JSON.parse(awsFile.Body.toString('utf-8'));
+        console.log("data structure is ",datastructure);
+        return datastructure;
+    }
+
+    async restructuring(body,datastructure){
+        
+        console.log("restricturing function is called");
+        const keys = Object.keys(datastructure);
+        const length = Object.keys(datastructure).length;
+        for (let i = 0, len = length ; i < len; i++) {
+            if (body[keys[i]]){
+                this.reformedData[datastructure[keys[i]]["new_name"]] = body[keys[i]];
+            }
+            try {
+                if (this.reformedData[datastructure[keys[i]]["new_name"]] !== null ) {
+                    console.log("bot altered");
+                }
+                
+            } catch (error) {
+
+                this.reformedData[datastructure[keys[i]]["new_name"]] = "null";
+            }
+
+        }
+        console.log("reformed_Data ",this.reformedData);
+        return this.reformedData;
+    }
+
+    kobotoolbox = async(req, res)=>{
+        console.log(req.body);
+        const fileName = req.body["_id"];
+        const [getFileKey, uploadFileKey] = await this.getKeys(req,fileName);
+        const datastructure = await this.getdatastructure(getFileKey);
+        const data = await this.restructuring(req.body,datastructure);
+        await this.awss3manager.uploadKoboData(uploadFileKey,data);
+        this.responseHandler.sendSuccessResponse(res, 200, 'Message is sent successfully!', "");
+
+    };
+}
--- src/api/routes/platform.webhook.routes.ts
+++ src/api/routes/platform.webhook.routes.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { Logger } from '../../common/logger';
 import { injectable } from 'tsyringe';
 import { ClientWebhookController } from '../controllers/client.webhook.controller';
+import { kobotoolboxController } from '../controllers/kobotoolbox.controller';
 import { ClientEnvironmentProviderService } from '../../services/set.client/client.environment.provider.service';
 import { DatabaseConnection } from '../../middleware/database.connection.middleware';
 
@@ -10,6 +11,7 @@ export class PlatformWebhookRoutes{
 
     constructor(private logger?: Logger,
         private _clientWebhookController?: ClientWebhookController,
+        private _kobotoolboxController?: kobotoolboxController,
         private clientEnvironmentProviderService?: ClientEnvironmentProviderService,
         private dbconnectioMiddleware?: DatabaseConnection){
         this.logger.log("Inside whatsapp Routes...");
@@ -21,6 +23,7 @@ export class PlatformWebhookRoutes{
         router.post(`/:client/:channel/:unique_token/receive`, this.clientEnvironmentProviderService.clientNameMiddleware, this.dbconnectioMiddleware.myLogger, this._clientWebhookController.receiveMessage);
         router.get(`/:client/:channel/:unique_token/webhook`, this.clientEnvironmentProviderService.clientNameMiddleware, this._clientWebhookController.authenticateMetaWhatsappWebhook);
         router.post(`/:client/:channel/:unique_token/webhook`, this.clientEnvironmentProviderService.clientNameMiddleware, this.dbconnectioMiddleware.metaDBConnection, this._clientWebhookController.receiveMessageMetaWhatsapp);
+        router.post(`/:client/:form_name/kobotoolbox/abcd/getData`, this.clientEnvironmentProviderService.clientNameMiddleware, this._kobotoolboxController.kobotoolbox );
         app.use('/v1/', router);
     }
 
