53ce2c0f1a2298c7382771ad44b3d89f31ccbfbd
Rohitkemprai93 <rohit.kemprai@kansocloud.com>
2023-03-22 08:42:35 +0530

    check the type of whatsapp id

--- src/middleware/check.cross.connection.ts
+++ src/middleware/check.cross.connection.ts
@@ -13,9 +13,16 @@ export class CheckCrossConnection {
         const set_phone_number_id = this.clientEnvironment.getClientEnvironmentVariable('WHATSAPP_PHONE_NUMBER_ID');
         const urlParsed = req.url.split('/');
         if (urlParsed.includes("whatsappMeta")) {
-            if (req.body.entry[0].changes[0].value.metadata.phone_number_id != set_phone_number_id){
+            const phone_number_id_in_request = req.body.entry[0].changes[0].value.metadata.phone_number_id;
+            console.log("phone_number_id_in_request:" + phone_number_id_in_request + " type is: " + typeof(phone_number_id_in_request));
+            console.log("set_phone_number_id:" + set_phone_number_id + " type is: " + typeof(set_phone_number_id));
+            if (phone_number_id_in_request !== set_phone_number_id){
                 this.responseHandler.sendSuccessResponse(res, 200, 'Cross Connection', "");
                 console.log("Cross connection");
+            }
+            else {
+                console.log("No cross connection");
+                next();
             }
         }
         else {
