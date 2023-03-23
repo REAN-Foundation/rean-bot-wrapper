d31a3b0f7c99b63ff40cadb9484d0014484fec3f
Rohitkemprai93 <90680174+Rohitkemprai93@users.noreply.github.com>
2023-03-23 16:05:15 +0530

    Bug/manasa random authentication issue (#277)
    * updated user stat service
    
    * printing the unique token that is failing in UAT
    
    * checking client whatsappnumberID from req and the and the set whatsappID in config is same or not before the req is authenticated
    
    * added a middleware to check cross connection
    
    * consolelog req url
    
    * passing the middleware as const function
    
    * linting issues
    
    * check if the req has phne number id
    
    * unequal
    
    * check for telegram
    
    * check for whatsappMeta
    
    * check the type of whatsapp id
    
    * resolve container
    
    * console log the client name
    
    * clientnamemiddleware needs to be called before checking cross connection
    
    * response handler autoinject
    
    * remove clientNameMiddleware from route level
    
    * set clientnamemiddleware only before check

--- src/middleware/check.cross.connection.ts
+++ src/middleware/check.cross.connection.ts
@@ -1,8 +1,9 @@
-import { autoInjectable, container } from "tsyringe";
+import { autoInjectable, container, singleton } from "tsyringe";
 import { ClientEnvironmentProviderService } from "../services/set.client/client.environment.provider.service";
 import { ResponseHandler } from '../utils/response.handler';
 
 @autoInjectable()
+@singleton()
 export class CheckCrossConnection {
 
     constructor(
@@ -12,9 +13,10 @@ export class CheckCrossConnection {
 
         // eslint-disable-next-line max-len
         const clientEnvironmentProviderService: ClientEnvironmentProviderService = container.resolve(ClientEnvironmentProviderService);
-        const set_phone_number_id = clientEnvironmentProviderService.getClientEnvironmentVariable('WHATSAPP_PHONE_NUMBER_ID');
         const urlParsed = req.url.split('/');
         if (urlParsed.includes("whatsappMeta")) {
+            clientEnvironmentProviderService.clientNameMiddleware(req, res, next);
+            const set_phone_number_id = clientEnvironmentProviderService.getClientEnvironmentVariable('WHATSAPP_PHONE_NUMBER_ID');
             const phone_number_id_in_request = req.body.entry[0].changes[0].value.metadata.phone_number_id;
             console.log("phone_number_id_in_request:" + phone_number_id_in_request + " type is: " + typeof(phone_number_id_in_request));
             console.log("set_phone_number_id:" + set_phone_number_id + " type is: " + typeof(set_phone_number_id));
