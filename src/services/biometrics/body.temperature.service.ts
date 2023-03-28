0fdef02484b4cee0766e7b4707d17a81829ef3cc
Rohitkemprai93 <rohit.kemprai@kansocloud.com>
2023-03-28 16:21:56 +0530

    reancare mobile app containerisation

--- src/services/biometrics/body.temperature.service.ts
+++ src/services/biometrics/body.temperature.service.ts
@@ -1,156 +1,161 @@
-import { getHeaders } from '../../services/biometrics/get.headers';
+import { GetHeaders } from '../../services/biometrics/get.headers';
 import { GetPatientInfoService } from "../../services/support.app.service";
 import { ClientEnvironmentProviderService } from '../set.client/client.environment.provider.service';
-import { container,  } from 'tsyringe';
+import { inject, Lifecycle, scoped } from 'tsyringe';
 import needle from "needle";
 
-const getPatientInfoService: GetPatientInfoService = container.resolve(GetPatientInfoService);
+@scoped(Lifecycle.ContainerScoped)
+export class BodyTemperatureService {
+    
+    constructor(
+        @inject(GetHeaders) private getHeaders: GetHeaders,
+        @inject(GetPatientInfoService) private getPatientInfoService: GetPatientInfoService,
+        // eslint-disable-next-line max-len
+        @inject(ClientEnvironmentProviderService) private clientEnvironmentProviderService: ClientEnvironmentProviderService
+    ){}
 
-let remark = '';
-const getremark = function (BodyTemperature_Unit,BodyTemperature) {
-
-    if (BodyTemperature_Unit === '°F') {
-                    
-        if (BodyTemperature < 95) {
-            remark = 'in low range.';
-        } else if (BodyTemperature < 99 && BodyTemperature > 95) {
-            remark = 'in normal range. Stay healthy!';
-        } else if (BodyTemperature < 100 && BodyTemperature >= 99) {
-            remark = 'mild fever.';
-        } else if (BodyTemperature < 102 && BodyTemperature >= 100) {
-            remark = 'high. Please consult your Doctor.';
-        } else if (BodyTemperature >= 102) {
-            remark = 'very high. Please consult your Doctor.';
+    getremark = (BodyTemperature_Unit,BodyTemperature) => {
+        let remark = '';
+        if (BodyTemperature_Unit === '°F') {
+            if (BodyTemperature < 95) {
+                remark = 'in low range.';
+            } else if (BodyTemperature < 99 && BodyTemperature > 95) {
+                remark = 'in normal range. Stay healthy!';
+            } else if (BodyTemperature < 100 && BodyTemperature >= 99) {
+                remark = 'mild fever.';
+            } else if (BodyTemperature < 102 && BodyTemperature >= 100) {
+                remark = 'high. Please consult your Doctor.';
+            } else if (BodyTemperature >= 102) {
+                remark = 'very high. Please consult your Doctor.';
+            }
         }
-    }
-    checkForC(BodyTemperature_Unit, BodyTemperature);
-    return remark;
-};
+        this.checkForC(BodyTemperature_Unit, BodyTemperature);
+        return remark;
+    };
 
-export const updateBodyTemperatureInfoService = async (eventObj) => {
+    updateBodyTemperatureInfoService = async (eventObj) => {
 
-    if (eventObj) {
-        const clientEnvironmentProviderService: ClientEnvironmentProviderService = container.resolve(
-            ClientEnvironmentProviderService);
-        var { patientUserId, accessToken, BodyTemperature_Unit, BodyTemperature } = await checkEntry(eventObj);
+        if (eventObj) {
+            var { patientUserId, accessToken, BodyTemperature_Unit, BodyTemperature } = await this.checkEntry(eventObj);
 
-        const ReanBackendBaseUrl = clientEnvironmentProviderService.getClientEnvironmentVariable("REAN_APP_BACKEND_BASE_URL");
-        const url = `${ReanBackendBaseUrl}clinical/biometrics/body-temperatures/search?patientUserId=${patientUserId}`;
-        
-        const options = getHeaders(accessToken);
-        const resp = await needle("get", url, options);
-        const bodyTemperatureId = resp.body.Data.BodyTemperatureRecords.Items[0].id;
-        let unitmsg = null;
-        ({ unitmsg, BodyTemperature_Unit } = getUnit(BodyTemperature_Unit,BodyTemperature));
-        const apiUrl = `${ReanBackendBaseUrl}clinical/biometrics/body-temperatures/${bodyTemperatureId}`;
+            const ReanBackendBaseUrl = this.clientEnvironmentProviderService.getClientEnvironmentVariable("REAN_APP_BACKEND_BASE_URL");
+            const url = `${ReanBackendBaseUrl}clinical/biometrics/body-temperatures/search?patientUserId=${patientUserId}`;
+            
+            const options = this.getHeaders.getHeaders(accessToken);
+            const resp = await needle("get", url, options);
+            const bodyTemperatureId = resp.body.Data.BodyTemperatureRecords.Items[0].id;
+            let unitmsg = null;
+            ({ unitmsg, BodyTemperature_Unit } = this.getUnit(BodyTemperature_Unit,BodyTemperature));
+            const apiUrl = `${ReanBackendBaseUrl}clinical/biometrics/body-temperatures/${bodyTemperatureId}`;
 
-        const obj = {
-            "PatientUserId"   : patientUserId,
-            "BodyTemperature" : BodyTemperature,
-            "Unit"            : BodyTemperature_Unit
-        };
+            const obj = {
+                "PatientUserId"   : patientUserId,
+                "BodyTemperature" : BodyTemperature,
+                "Unit"            : BodyTemperature_Unit
+            };
 
-        const response = await needle("put", apiUrl, obj, options);
+            const response = await needle("put", apiUrl, obj, options);
 
-        if (response.statusCode !== 200) {
-            throw new Error("Failed to get response from API.");
-        }
-        remark = getremark(BodyTemperature_Unit,BodyTemperature);
-        const t = response.body.Data.BodyTemperature.BodyTemperature;
-        const u = response.body.Data.BodyTemperature.Unit;
+            if (response.statusCode !== 200) {
+                throw new Error("Failed to get response from API.");
+            }
+            const remark = this.getremark(BodyTemperature_Unit,BodyTemperature);
+            const t = response.body.Data.BodyTemperature.BodyTemperature;
+            const u = response.body.Data.BodyTemperature.Unit;
 
-        const dffMessage = `${unitmsg}Your updated body temperature ${t} ${u} is ${remark}`;
+            const dffMessage = `${unitmsg}Your updated body temperature ${t} ${u} is ${remark}`;
 
-        const data = { "fulfillmentMessages": [{ "text": { "text": [dffMessage] } }] };
+            const data = { "fulfillmentMessages": [{ "text": { "text": [dffMessage] } }] };
 
-        return { sendDff: true, message: data };
-    } else {
-        throw new Error(`500, BodyTemperatureUpdate Info Service Error!`);
-    }
-};
-
-export const createBodyTemperatureInfoService = async (eventObj) => {
-
-    if (eventObj) {
-        const clientEnvironmentProviderService: ClientEnvironmentProviderService = container.resolve(
-            ClientEnvironmentProviderService);
-        var { patientUserId, accessToken, BodyTemperature_Unit, BodyTemperature } = await checkEntry(eventObj);
-        
-        let unitmsg = null;
-        ({ unitmsg, BodyTemperature_Unit } = getUnit(BodyTemperature_Unit,BodyTemperature));
-        const ReanBackendBaseUrl = clientEnvironmentProviderService.getClientEnvironmentVariable("REAN_APP_BACKEND_BASE_URL");
-        const options = getHeaders(accessToken);
-        const apiUrl = `${ReanBackendBaseUrl}clinical/biometrics/body-temperatures`;
-
-        const obj = {
-            "PatientUserId"   : patientUserId,
-            "BodyTemperature" : BodyTemperature,
-            "Unit"            : BodyTemperature_Unit,
-            "RecordDate"      : Date()
-        };
-
-        const response = await needle("post", apiUrl, obj, options);
-
-        if (response.statusCode !== 201) {
-            throw new Error("Failed to get response from API.");
-        }
-        remark = getremark(BodyTemperature_Unit,BodyTemperature);
-        const t = response.body.Data.BodyTemperature.BodyTemperature;
-        const u = response.body.Data.BodyTemperature.Unit;
-
-        const dffMessage = `${unitmsg}Your newly added body temperature ${t} ${u} is ${remark}`;
-
-        const data = { "fulfillmentMessages": [{ "text": { "text": [dffMessage] } }] };
-
-        return { sendDff: true, message: data };
-    } else {
-        throw new Error(`500, BodyTemperatureCreate Info Service Error!`);
-    }
-};
-
-function getUnit(BodyTemperature_Unit: any, BodyTemperature: any) {
-    let unitmsg = '';
-    if (BodyTemperature_Unit === '') {
-
-        if (BodyTemperature < 50) {
-            BodyTemperature_Unit = '°C';
-            unitmsg = `Temperature unit assumed to °C. `;
+            return { sendDff: true, message: data };
         } else {
-            BodyTemperature_Unit = '°F';
-            unitmsg = `Temperature unit assumed to °F. `;
+            throw new Error(`500, BodyTemperatureUpdate Info Service Error!`);
         }
+    };
 
-    }
-    return { unitmsg, BodyTemperature_Unit };
-}
+    createBodyTemperatureInfoService = async (eventObj) => {
 
-async function checkEntry(eventObj: any) {
-    const BodyTemperature = eventObj.body.queryResult.parameters.BodyTemperature;
-    const BodyTemperature_Unit = eventObj.body.queryResult.parameters.Unit;
+        if (eventObj) {
+            var { patientUserId, accessToken, BodyTemperature_Unit, BodyTemperature } = await this.checkEntry(eventObj);
+            
+            let unitmsg = null;
+            ({ unitmsg, BodyTemperature_Unit } = this.getUnit(BodyTemperature_Unit,BodyTemperature));
+            const ReanBackendBaseUrl = this.clientEnvironmentProviderService.getClientEnvironmentVariable("REAN_APP_BACKEND_BASE_URL");
+            const options = this.getHeaders.getHeaders(accessToken);
+            const apiUrl = `${ReanBackendBaseUrl}clinical/biometrics/body-temperatures`;
 
-    if (!BodyTemperature) {
-        throw new Error("Missing required parameter BodyTemperature");
-    }
-    let result = null;
-    result = await getPatientInfoService.getPatientsByPhoneNumberservice(eventObj);
+            const obj = {
+                "PatientUserId"   : patientUserId,
+                "BodyTemperature" : BodyTemperature,
+                "Unit"            : BodyTemperature_Unit,
+                "RecordDate"      : Date()
+            };
 
-    const patientUserId = result.message[0].UserId;
+            const response = await needle("post", apiUrl, obj, options);
 
-    const accessToken = result.message[0].accessToken;
-    return { patientUserId, accessToken, BodyTemperature_Unit, BodyTemperature };
-}
+            if (response.statusCode !== 201) {
+                throw new Error("Failed to get response from API.");
+            }
+            const remark = this.getremark(BodyTemperature_Unit,BodyTemperature);
+            const t = response.body.Data.BodyTemperature.BodyTemperature;
+            const u = response.body.Data.BodyTemperature.Unit;
 
-function checkForC(BodyTemperature_Unit: any, BodyTemperature: any) {
-    if (BodyTemperature_Unit === '°C') {
+            const dffMessage = `${unitmsg}Your newly added body temperature ${t} ${u} is ${remark}`;
 
-        if (BodyTemperature < 37) {
-            remark = 'in low range.';
-        } else if (BodyTemperature <= 38) {
-            remark = 'in normal range. Stay healthy!';
-        } else if (BodyTemperature < 39) {
-            remark = 'mild fever.';
-        } else if (BodyTemperature < 42) {
-            remark = 'very high. Please consult your Doctor.';
+            const data = { "fulfillmentMessages": [{ "text": { "text": [dffMessage] } }] };
+
+            return { sendDff: true, message: data };
+        } else {
+            throw new Error(`500, BodyTemperatureCreate Info Service Error!`);
         }
+    };
+
+    getUnit(BodyTemperature_Unit: any, BodyTemperature: any) {
+        let unitmsg = '';
+        if (BodyTemperature_Unit === '') {
+
+            if (BodyTemperature < 50) {
+                BodyTemperature_Unit = '°C';
+                unitmsg = `Temperature unit assumed to °C. `;
+            } else {
+                BodyTemperature_Unit = '°F';
+                unitmsg = `Temperature unit assumed to °F. `;
+            }
+
+        }
+        return { unitmsg, BodyTemperature_Unit };
+    }
+
+    async checkEntry(eventObj: any) {
+        const BodyTemperature = eventObj.body.queryResult.parameters.BodyTemperature;
+        const BodyTemperature_Unit = eventObj.body.queryResult.parameters.Unit;
+
+        if (!BodyTemperature) {
+            throw new Error("Missing required parameter BodyTemperature");
+        }
+        let result = null;
+        result = await this.getPatientInfoService.getPatientsByPhoneNumberservice(eventObj);
+
+        const patientUserId = result.message[0].UserId;
+
+        const accessToken = result.message[0].accessToken;
+        return { patientUserId, accessToken, BodyTemperature_Unit, BodyTemperature };
+    }
+
+    checkForC(BodyTemperature_Unit: any, BodyTemperature: any) {
+        let remark = '';
+        if (BodyTemperature_Unit === '°C') {
+            if (BodyTemperature < 37) {
+                remark = 'in low range.';
+            } else if (BodyTemperature <= 38) {
+                remark = 'in normal range. Stay healthy!';
+            } else if (BodyTemperature < 39) {
+                remark = 'mild fever.';
+            } else if (BodyTemperature < 42) {
+                remark = 'very high. Please consult your Doctor.';
+            }
+        }
+        return remark;
     }
 }
+
--- src/services/biometrics/pulse.service.ts
+++ src/services/biometrics/pulse.service.ts
@@ -1,118 +1,123 @@
-import { getHeaders } from '../../services/biometrics/get.headers';
+import { GetHeaders } from '../../services/biometrics/get.headers';
 import { GetPatientInfoService } from "../../services/support.app.service";
 import { ClientEnvironmentProviderService } from '../set.client/client.environment.provider.service';
-import { container,  } from 'tsyringe';
+import { inject, Lifecycle, scoped } from 'tsyringe';
 import needle from "needle";
 
-const getPatientInfoService: GetPatientInfoService = container.resolve(GetPatientInfoService);
+@scoped(Lifecycle.ContainerScoped)
+export class PulseService {
 
-let remark = '';
-const getremark = function (Pulse) {
+    constructor(
+        @inject(GetHeaders) private getHeaders: GetHeaders,
+        @inject(GetPatientInfoService) private getPatientInfoService: GetPatientInfoService,
+        // eslint-disable-next-line max-len
+        @inject(ClientEnvironmentProviderService) private clientEnvironmentProviderService: ClientEnvironmentProviderService
+    ){}
 
-    if (Pulse <= 100 && Pulse > 40) {
-        remark = 'in normal range. Stay healthy!';
-    } else if (Pulse <= 109 && Pulse > 100) {
-        remark = 'little high. We suggest please continue home monitoring.';
-    } else if (Pulse <= 130 && Pulse > 109) {
-        remark = 'high. Please consult your Doctor.';
-    } else if (Pulse > 130) {
-        remark = 'very high. Please consult your Doctor.';
-    }
-    return remark;
-};
-
-export const updatePulseInfoService = async (eventObj) => {
-
-    if (eventObj) {
-        const clientEnvironmentProviderService: ClientEnvironmentProviderService = container.resolve(
-            ClientEnvironmentProviderService);
-        var { patientUserId, accessToken, Pulse_Unit, Pulse } = await checkEntry(eventObj);
-
-        const ReanBackendBaseUrl = clientEnvironmentProviderService.getClientEnvironmentVariable("REAN_APP_BACKEND_BASE_URL");
-        const url = `${ReanBackendBaseUrl}clinical/biometrics/pulse/search?patientUserId=${patientUserId}`;
-        
-        const options = getHeaders(accessToken);
-        const resp = await needle("get", url, options);
-        const pulseId = resp.body.Data.PulseRecords.Items[0].id;
-        
-        const apiUrl = `${ReanBackendBaseUrl}clinical/biometrics/Pulse/${pulseId}`;
-
-        const obj = {
-            "PatientUserId" : patientUserId,
-            "Pulse"         : Pulse,
-            "Unit"          : Pulse_Unit
-        };
-
-        const response = await needle("put", apiUrl, obj, options);
-
-        if (response.statusCode !== 200) {
-            throw new Error("Failed to get response from API.");
+    getremark = (Pulse) => {
+        let remark = '';
+        if (Pulse <= 100 && Pulse > 40) {
+            remark = 'in normal range. Stay healthy!';
+        } else if (Pulse <= 109 && Pulse > 100) {
+            remark = 'little high. We suggest please continue home monitoring.';
+        } else if (Pulse <= 130 && Pulse > 109) {
+            remark = 'high. Please consult your Doctor.';
+        } else if (Pulse > 130) {
+            remark = 'very high. Please consult your Doctor.';
         }
-        remark = getremark(Pulse);
-        const p = response.body.Data.Pulse.Pulse;
+        return remark;
+    };
 
-        const dffMessage = `Your updated pulse rate ${p} ${response.body.Data.Pulse.Unit} is ${remark}`;
+    updatePulseInfoService = async (eventObj) => {
 
-        const data = { "fulfillmentMessages": [{ "text": { "text": [dffMessage] } }] };
+        if (eventObj) {
+            var { patientUserId, accessToken, Pulse_Unit, Pulse } = await this.checkEntry(eventObj);
 
-        return { sendDff: true, message: data };
-    } else {
-        throw new Error(`500, PulseUpdate Info Service Error!`);
-    }
-};
+            const ReanBackendBaseUrl = this.clientEnvironmentProviderService.getClientEnvironmentVariable("REAN_APP_BACKEND_BASE_URL");
+            const url = `${ReanBackendBaseUrl}clinical/biometrics/pulse/search?patientUserId=${patientUserId}`;
+            
+            const options = this.getHeaders.getHeaders(accessToken);
+            const resp = await needle("get", url, options);
+            const pulseId = resp.body.Data.PulseRecords.Items[0].id;
+            
+            const apiUrl = `${ReanBackendBaseUrl}clinical/biometrics/Pulse/${pulseId}`;
 
-export const createPulseInfoService = async (eventObj) => {
+            const obj = {
+                "PatientUserId" : patientUserId,
+                "Pulse"         : Pulse,
+                "Unit"          : Pulse_Unit
+            };
 
-    if (eventObj) {
-        const clientEnvironmentProviderService: ClientEnvironmentProviderService = container.resolve(
-            ClientEnvironmentProviderService);
-        var { patientUserId, accessToken, Pulse_Unit, Pulse } = await checkEntry(eventObj);
-        
-        const ReanBackendBaseUrl = clientEnvironmentProviderService.getClientEnvironmentVariable("REAN_APP_BACKEND_BASE_URL");
-        const options = getHeaders(accessToken);
-        const apiUrl = `${ReanBackendBaseUrl}clinical/biometrics/Pulse`;
+            const response = await needle("put", apiUrl, obj, options);
 
-        const obj = {
-            "PatientUserId" : patientUserId,
-            "Pulse"         : Pulse,
-            "Unit"          : Pulse_Unit,
-            "RecordDate"    : Date()
-        };
+            if (response.statusCode !== 200) {
+                throw new Error("Failed to get response from API.");
+            }
+            const remark = this.getremark(Pulse);
+            const p = response.body.Data.Pulse.Pulse;
 
-        const response = await needle("post", apiUrl, obj, options);
+            const dffMessage = `Your updated pulse rate ${p} ${response.body.Data.Pulse.Unit} is ${remark}`;
 
-        if (response.statusCode !== 201) {
-            throw new Error("Failed to get response from API.");
+            const data = { "fulfillmentMessages": [{ "text": { "text": [dffMessage] } }] };
+
+            return { sendDff: true, message: data };
+        } else {
+            throw new Error(`500, PulseUpdate Info Service Error!`);
         }
-        remark = getremark(Pulse);
-        const p = response.body.Data.Pulse.Pulse;
+    };
 
-        const dffMessage = `Your newly added pulse rate ${p} ${response.body.Data.Pulse.Unit} is ${remark}`;
+    createPulseInfoService = async (eventObj) => {
 
-        const data = { "fulfillmentMessages": [{ "text": { "text": [dffMessage] } }] };
+        if (eventObj) {
+            var { patientUserId, accessToken, Pulse_Unit, Pulse } = await this.checkEntry(eventObj);
+            
+            const ReanBackendBaseUrl = this.clientEnvironmentProviderService.getClientEnvironmentVariable("REAN_APP_BACKEND_BASE_URL");
+            const options = this.getHeaders.getHeaders(accessToken);
+            const apiUrl = `${ReanBackendBaseUrl}clinical/biometrics/Pulse`;
 
-        return { sendDff: true, message: data };
-    } else {
-        throw new Error(`500, PulseCreate Info Service Error!`);
+            const obj = {
+                "PatientUserId" : patientUserId,
+                "Pulse"         : Pulse,
+                "Unit"          : Pulse_Unit,
+                "RecordDate"    : Date()
+            };
+
+            const response = await needle("post", apiUrl, obj, options);
+
+            if (response.statusCode !== 201) {
+                throw new Error("Failed to get response from API.");
+            }
+            const remark = this.getremark(Pulse);
+            const p = response.body.Data.Pulse.Pulse;
+
+            const dffMessage = `Your newly added pulse rate ${p} ${response.body.Data.Pulse.Unit} is ${remark}`;
+
+            const data = { "fulfillmentMessages": [{ "text": { "text": [dffMessage] } }] };
+
+            return { sendDff: true, message: data };
+        } else {
+            throw new Error(`500, PulseCreate Info Service Error!`);
+        }
+    };
+
+    async checkEntry(eventObj: any) {
+        const Pulse = eventObj.body.queryResult.parameters.Pulse;
+        let Pulse_Unit = eventObj.body.queryResult.parameters.Pulse_Unit;
+
+        if (Pulse_Unit === '') {
+            Pulse_Unit = 'bpm';
+        }
+
+        if (!Pulse) {
+            throw new Error("Missing required parameter pulse");
+        }
+        let result = null;
+        result = await this.getPatientInfoService.getPatientsByPhoneNumberservice(eventObj);
+
+        const patientUserId = result.message[0].UserId;
+
+        const accessToken = result.message[0].accessToken;
+        return { patientUserId, accessToken, Pulse_Unit, Pulse };
     }
-};
 
-async function checkEntry(eventObj: any) {
-    const Pulse = eventObj.body.queryResult.parameters.Pulse;
-    let Pulse_Unit = eventObj.body.queryResult.parameters.Pulse_Unit;
-
-    if (Pulse_Unit === '') {
-        Pulse_Unit = 'bpm';
-    }
-
-    if (!Pulse) {
-        throw new Error("Missing required parameter pulse");
-    }
-    let result = null;
-    result = await getPatientInfoService.getPatientsByPhoneNumberservice(eventObj);
-
-    const patientUserId = result.message[0].UserId;
-
-    const accessToken = result.message[0].accessToken;
-    return { patientUserId, accessToken, Pulse_Unit, Pulse };
 }
