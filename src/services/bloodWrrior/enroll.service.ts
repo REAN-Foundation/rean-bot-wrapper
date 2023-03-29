47c31b244f3bb00cfbb1ee2c0d5df2c11e82fd3b
Rohitkemprai93 <rohit.kemprai@kansocloud.com>
2023-03-29 13:27:43 +0530

    removed async from promise

--- src/services/bloodWrrior/enroll.service.ts
+++ src/services/bloodWrrior/enroll.service.ts
@@ -16,42 +16,40 @@ export class EnrollPatientService {
     ){}
 
     enrollPatientService = async (eventObj) => {
-        return new Promise(async (resolve) => {
-            try {
-                let result = null;
-                result = await this.getPatientInfoService.getPatientsByPhoneNumberservice(eventObj);
+        try {
+            let result = null;
+            result = await this.getPatientInfoService.getPatientsByPhoneNumberservice(eventObj);
         
-                const patientUserId = result.message[0].UserId;
-                const accessToken = result.message[0].accessToken;
-                const options = this.getHeaders.getHeaders(accessToken);
+            const patientUserId = result.message[0].UserId;
+            const accessToken = result.message[0].accessToken;
+            const options = this.getHeaders.getHeaders(accessToken);
         
-                //Update patient information
-                const ReanBackendBaseUrl =
+            //Update patient information
+            const ReanBackendBaseUrl =
                     this.clientEnvironmentProviderService.getClientEnvironmentVariable('REAN_APP_BACKEND_BASE_URL');
         
-                const url = `${ReanBackendBaseUrl}care-plans/patients/${patientUserId}/enroll`;
-                const obj1 = {
-                    Provider  : "REAN_BW",
-                    PlanName  : "Patient messages",
-                    PlanCode  : "Patient-Reminders",
-                    StartDate : new Date().toISOString().split('T')[0]
-                };
+            const url = `${ReanBackendBaseUrl}care-plans/patients/${patientUserId}/enroll`;
+            const obj1 = {
+                Provider  : "REAN_BW",
+                PlanName  : "Patient messages",
+                PlanCode  : "Patient-Reminders",
+                StartDate : new Date().toISOString().split('T')[0]
+            };
         
-                const resp = await needle('post', url, obj1, options);
-                if (resp.statusCode !== 201) {
-                    throw new Error('Failed to get response from ReanCare service API.');
-                }
-                const enrollmentId = resp.body.Data.Enrollment.EnrollmentId;
-                Logger.instance().log(`Enrollment id of user is: ${enrollmentId}`);
-        
-                const dffMessage = `Thank you for confirmation. We will remind you before expected blood transfusion date.`;
-                resolve( { sendDff: true, message: { fulfillmentMessages: [{ text: { text: [dffMessage] } }] } });
-    
-            } catch (error) {
-                Logger.instance()
-                    .log_error(error.message,500,'Register patient with blood warrior messaging service error');
+            const resp = await needle('post', url, obj1, options);
+            if (resp.statusCode !== 201) {
+                throw new Error('Failed to get response from ReanCare service API.');
             }
-        });
+            const enrollmentId = resp.body.Data.Enrollment.EnrollmentId;
+            Logger.instance().log(`Enrollment id of user is: ${enrollmentId}`);
+        
+            const dffMessage = `Thank you for confirmation. We will remind you before expected blood transfusion date.`;
+            return ( { sendDff: true, message: { fulfillmentMessages: [{ text: { text: [dffMessage] } }] } });
+    
+        } catch (error) {
+            Logger.instance()
+                .log_error(error.message,500,'Register patient with blood warrior messaging service error');
+        }
     };
     
 }
