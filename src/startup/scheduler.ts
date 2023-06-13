4c3b84cac8d1b1265ee4b1817b7c4f130db5ae50
Pallavi <pallavi.sachdeva@reanfoundation.org>
2023-06-13 10:59:02 +0530

    CRON setup for penplus data backup
    i have made change to set the cron functionality that runs everyday at 12:00 am mudnught

--- src/startup/scheduler.ts
+++ src/startup/scheduler.ts
@@ -1,10 +1,20 @@
 import { Logger } from '../common/logger';
+import * as cron from 'node-cron';
+import * as CronSchedules from '../assets/seed.data/cron.schedules.json';
+import { Loader } from './loader';
+import {databackup} from '../services/scheduleDataBackup.service';
 
 export class Scheduler {
 
     private static _instance: Scheduler = null;
 
+    private static envSchedules = null;
+
+    private static env = null;
+
     private constructor() {
+        Scheduler.env = process.env.ENVIRONMENT;
+        Scheduler.envSchedules = CronSchedules[Scheduler.env];
         Logger.instance().log('Initializing the schedular.');
     }
 
@@ -15,6 +25,8 @@ export class Scheduler {
     public schedule = async (): Promise<boolean> => {
         return new Promise((resolve, reject) => {
             try {
+                console.log("starting the schedular service");
+                this.scheduleDataBackup();
                 resolve(true);
             } catch (error) {
                 Logger.instance().log('Error initializing the schedular.: ' + error.message);
@@ -22,5 +34,21 @@ export class Scheduler {
             }
         });
     };
+    private scheduleDataBackup = () => {
+        for (const clientName in Scheduler.envSchedules){
+            cron.schedule(Scheduler.envSchedules[clientName]['scheduleDataBackup'], () => {
+                (async () => {
+                    Logger.instance().log(`Running scheducled jobs: DataBackup in S3 ${clientName}`);
+                    var databackupobj = new databackup();
+                    await databackupobj.main(clientName);
+
+                    // var service = Loader.container.resolve(FileResourceService);
+                    // await service.cleanupTempFiles();
+                })();
+            });
+        }
+
+    };
+
 
 }
