e94b27956aa93e71651c4a6165fc04983ea714bb
sushant-thotakura <sushant.thotakura@kansocloud.com>
2023-07-12 15:11:09 +0530

    Update to demo bot service

--- src/services/demoBot.service.ts
+++ src/services/demoBot.service.ts
@@ -142,7 +142,8 @@ export class demoBotService {
                 console.log(`Intent - ${displayName} - ${response.name} created`);
             }
         }
-        
+
+        console.log("All intents created");
         return true;
 
     }
