5fd1a5c11caee8f7401f90dd370eb38275b53ea5
sushant-thotakura <sushant.thotakura@kansocloud.com>
2024-03-21 16:35:41 +0530

    Updated code for package changes
    - Removed the forked repository in the code as it was causing vulnerabilities.

--- src/intentEmitters/intentListeners/genericpedia.listener.ts
+++ src/intentEmitters/intentListeners/genericpedia.listener.ts
@@ -1,43 +1,43 @@
-import { Logger } from '../../common/logger';
-import { getGenericpediaservice, getGenericpediaChemistservice } from "../../services/genericpedia.service";
+// import { Logger } from '../../common/logger';
+// import { getGenericpediaservice, getGenericpediaChemistservice } from "../../services/genericpedia.service";
 
-export const getGenericpedia = async (intent, eventObj) => {
-    try {
-        Logger.instance().log('Calling genericpedia Service to get available medicines');
+// export const getGenericpedia = async (intent, eventObj) => {
+//     try {
+//         Logger.instance().log('Calling genericpedia Service to get available medicines');
 
-        const res = 5;
-        const response = await getGenericpediaservice(eventObj, res);
+//         const res = 5;
+//         const response = await getGenericpediaservice(eventObj, res);
         
-        if (!response) {
-            console.log("I am failed");
-            throw new Error("Genericpedia Service Failed");
-        }
+//         if (!response) {
+//             console.log("I am failed");
+//             throw new Error("Genericpedia Service Failed");
+//         }
 
-        return response;
+//         return response;
 
-    } catch (error) {
-        Logger.instance().log_error(error.message, 500, "Genericpedia Listener Error!");
-        throw new Error("Genericpedia listener error");
-    }
-};
+//     } catch (error) {
+//         Logger.instance().log_error(error.message, 500, "Genericpedia Listener Error!");
+//         throw new Error("Genericpedia listener error");
+//     }
+// };
 
-export const getGenericpediaChemist = async (intent, eventObj) => {
-    try {
-        console.log('Calling genericpedia Service to get available medicines');
+// export const getGenericpediaChemist = async (intent, eventObj) => {
+//     try {
+//         console.log('Calling genericpedia Service to get available medicines');
 
-        const res = 5;
-        const response = await getGenericpediaChemistservice(eventObj, res);
+//         const res = 5;
+//         const response = await getGenericpediaChemistservice(eventObj, res);
         
-        if (!response) {
-            console.log("I am failed");
-            throw new Error("Genericpedia Service Failed");
-        }
+//         if (!response) {
+//             console.log("I am failed");
+//             throw new Error("Genericpedia Service Failed");
+//         }
 
-        return response;
+//         return response;
 
-    } catch (error) {
-        Logger.instance().log_error(error.message, 500, "Genericpedia Listener Error!");
-        throw new Error("Genericpedia listener error");
-    }
-};
+//     } catch (error) {
+//         Logger.instance().log_error(error.message, 500, "Genericpedia Listener Error!");
+//         throw new Error("Genericpedia listener error");
+//     }
+// };
 
