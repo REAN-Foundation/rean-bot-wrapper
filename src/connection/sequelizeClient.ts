14ec9cb5e642353514646a8c40d83e5fe71793dc
sushant-thotakura <sushant.thotakura@reanfoundation.org>
2025-05-07 12:06:24 -0600

    Updates
    - Updated files for merge conflicts

--- src/connection/sequelizeClient.ts
+++ src/connection/sequelizeClient.ts
@@ -13,6 +13,7 @@ import { UserConsent } from '../models/user.consent.model';
 import { UserInfo } from '../models/user.info.model';
 import { Logger } from '../../src/common/logger';
 import WorkflowUserData from '../models/workflow.user.data.model';
+import { Intents } from '../models/intents/intents.model';
 
 //////////////////////////////////////////////////////////////////////////////////////
 
@@ -51,7 +52,7 @@ export class SequelizeClient {
                     UserConsent,
                     MessageStatus,
                     WorkflowUserData,
-                    UserInfo
+                    Intents
                 ]);
             }
 
