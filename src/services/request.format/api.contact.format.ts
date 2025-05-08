a87c6a6d6520c62e7eb2f02156cda6373b8ed2b3
AyushPurohit1352 <92979832+AyushPurohit1352@users.noreply.github.com>
2025-05-08 16:34:49 +0530

    Testing endpoint (#678)
    * added message format handling for different message formats
    
    * Added request format and interface for ApiChannel
    
    * changed authentication method for mock channekl
    
    * changed mock channel to api
    
    * Resolved the issues for api requests
    
    - Added api.messagetodialogflow
    - Updated the valid channels in client webhook controller
    
    ---------
    
    Co-authored-by: sushant-thotakura <sushant.thotakura@reanfoundation.org>

---
+++ src/services/request.format/api.contact.format.ts
@@ -0,0 +1,18 @@
+import { IApiRequestContactsEntities } from './api.request.interface';
+export class Contacts implements IApiRequestContactsEntities{
+
+    constructor(list) { this.list = list; }
+
+    private list;
+
+    getUserName() {
+        const username:string = this.list.profile.name;
+        return username;
+    }
+
+    getPlatformId() {
+        const platformId:string = this.list.id;
+        return platformId;
+    }
+
+}
