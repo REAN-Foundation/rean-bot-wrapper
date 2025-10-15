81a8747f80cf5ea193509fbf6b53c4a3b3d21afb
Pallavi17101997 <112403629+Pallavi17101997@users.noreply.github.com>
2025-10-15 17:16:57 +0530

    Symtom flow changes (#777)
    * LVPEI_changes
    
    * Updated handling using Cache
    
    - Added symptoms to cache to compare
    - Handled the no intent when additional symptoms are requested.
    
    * changes in the no flow and solved few errors of the existing flow
    
    * linting issues resolved
    
    * linting issues resolved
    
    ---------
    
    Co-authored-by: sushant-thotakura <sushant.thotakura@reanfoundation.org>

--- src/intentEmitters/intentListeners/kerotoplasty.bot.condition.Identification.listener.ts
+++ src/intentEmitters/intentListeners/kerotoplasty.bot.condition.Identification.listener.ts
@@ -1,34 +1,81 @@
-
 import { sendExtraMessages } from "../../services/send.extra.messages.service";
 import { Logger } from "../../common/logger";
 import { kerotoplastyService } from "../../services/kerotoplasty.service";
+import { dialoflowMessageFormatting } from "../../services/Dialogflow.service";
+import { CacheMemory } from "../../services/cache.memory.service";
 
-export const kerotoplastyConditionIdentificationListener = async (intent, eventObj) => {
-
-    const kerotoplastyServiceObj: kerotoplastyService = eventObj.container.resolve(kerotoplastyService);
+export const kerotoplastySymptomAnalysisListener = async (intent, eventObj) => {
     try {
-        const condition = await kerotoplastyServiceObj.identifyCondition(eventObj);
-        const [response,severityGrade] = await kerotoplastyServiceObj.conditionSpecificResponse(condition);
-        askingForImage(intent,eventObj,severityGrade);
-        return response;
+        const kerotoplastyServiceObj: kerotoplastyService = eventObj.container.resolve(kerotoplastyService);
+        const dialogflowService: dialoflowMessageFormatting = eventObj.container.resolve(dialoflowMessageFormatting);
+        const [symptoms, message, priority] =
+            await kerotoplastyServiceObj.identifyCondition(eventObj) as [string[], any, number];
+        
+        kerotoplastyServiceObj.postingOnClickup(intent, eventObj, priority);
+        let outputMessage = "To better understand your condition, we’ll ask you a few quick questions. This will help us guide you effectively.";
+        if (priority <= 1){
+            outputMessage =  message;
+        } else if (priority > 1 && intent === "symptomAnalysis") {
+
+            // Format symptoms list naturally (comma + "and")
+
+            const formattedSymptoms =
+                    symptoms.length > 1
+                        ? symptoms.slice(0, -1).join(", ") + " and " + symptoms.slice(-1)
+                        : symptoms[0] || "";
+
+            outputMessage = `Thanks for sharing your symptoms: *${formattedSymptoms}*`;
+
+        } else {
+            outputMessage = message;
+            // data = await dialogflowService.making_response(outputMessage);
+            // return data;
+        }
+        followUpStep(intent, eventObj, priority);
+        const data = await dialogflowService.making_response(outputMessage);
+        return data;
     }
-    catch (error) {
+    catch (error: any) {
         Logger.instance()
-            .log_error(error.message, 500, 'kerotoplasty_bot_condition identification Listener Error!');
+            .log_error(error.message, 500, "kerotoplasty_bot_condition identification Listener Error!");
         throw new Error("Keratoplasty bot condition identification Listener Error");
     }
 };
-async function askingForImage(intent,eventObj,severityGrade) {
+
+async function followUpStep(intent: string, eventObj: any, priority: number) {
     try {
-        const kerotoplastyServiceObj: kerotoplastyService = eventObj.container.resolve(kerotoplastyService);
-        const sendExtraMessagesobj: sendExtraMessages = eventObj.container.resolve(sendExtraMessages);
-        const inputMessage =  "Would you like to send an image of the affected area for the doctor's reference?";
-        const yesIntentName = "EyeImage";
-        const noIntentName = "responseNo";
-        sendExtraMessagesobj.sendSecondaryButtonMessage(inputMessage, yesIntentName, noIntentName,  eventObj);
-        kerotoplastyServiceObj.postingOnClickup(intent,eventObj,severityGrade);
+        const sendExtraMessagesObj: sendExtraMessages = eventObj.container.resolve(sendExtraMessages);
+
+        let inputMessage: string;
+        let yesIntentName: string;
+        let noIntentName: string;
+
+        // Variables for cache
+        const userPlatformId = eventObj.body.originalDetectIntentRequest.payload.userId;
+        const cacheKey = `SymptomsStorage:${userPlatformId}`;
+
+        if (priority <= 1 || intent === "MoreSymptoms" || intent === "KerotoplastyFollowUp") {
+            inputMessage = "Would you be able to provide an *image of the affected area of your eye* for the doctor’s assessment?";
+            yesIntentName = "EyeImage";
+            noIntentName = "responseNo";
+
+            // Delete the cache key as this is the final step
+            await CacheMemory.delete(cacheKey);
+        } else {
+            inputMessage = `Could you let me know if you have any other symptoms?`;
+            yesIntentName = "MoreSymptoms";
+            noIntentName = "KerotoplastyFollowUp";
+        }
+
+        await sendExtraMessagesObj.sendSecondaryButtonMessage(
+            inputMessage,
+            yesIntentName,
+            noIntentName,
+            eventObj
+        );
+
     } catch (error) {
-        console.log(error);
+        console.error("Error in followUpStep:", error);
         throw new Error("Keratoplasty next steps error");
     }
 }
