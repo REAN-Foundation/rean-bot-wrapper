1e719ba151576eba389e3948d89ed4d9b2d0d418
sushant-thotakura1 <sushant.thotakura@reanfoundation.org>
2025-11-27 23:12:34 -0700

    Deleted unwanted database files (#787)

--- src/refactor/interface/assessment/assessment.questions.interface.ts
+++
@@ -1,9 +0,0 @@
-export interface IAssessmentQuestions {
-    id : number;
-    assessmentQuestionId: string;
-    assessmentId: string;
-    assessmentTemplateId: string;
-    questions: string;
-    options: string;
-    platformMessageId: string;
-}
